6dc44abaeae383caab0238e878fb90fcf8bd7281
Richard Chov <richardchov@Richard-Chovs-MacBook-Pro.local>
2017-12-14 08:39:00 -0800

    movie is not working

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var client = new twitter(apiKeys.twitterKeys);
 var spotifyApiKey = new spotify(apiKeys.spotifyKeys);
 
 var command = process.argv[2];
-var spotifySearch = process.argv[3];
+var searchTerm = process.argv[3];
 
 function switchCommand() {
 switch(command){
@@ -55,9 +55,9 @@ switch(command){
 function spotifyThisSong(){
 // var spotify = new Spotify({spotifyApiKey});
   console.log('Music time!')
-  if (spotifySearch === undefined || spotifySearch ==="")
+  if (searchTerm === undefined || searchTerm ==="")
     console.log('No song selected, oh well too bad. Here is a song for you');
-    spotifySearch = 'The Sign Ace of Base'
+    searchTerm = 'The Sign Ace of Base'
 };
 
 spotifyApiKey.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
@@ -75,17 +75,55 @@ spotifyApiKey.search({ type: 'track', query: 'All the Small Things' }, function(
 console.log(data); 
 });
 
+spotifyThisSong();
 
 
+//-------------------------------OMDB
+
+function movieThis(){
+  if (searchTerm === undefined || searchTerm === "")
+    console.log("You didn't insert a movie title, but here you go.")
+    searchTerm = 'Mr.Nobody'
+};
+
+var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body){
+  if (!error && response.statusCode === 200){
+    var info = JSON.parse(body);
+    console.log(
+      "Title " + info.Title + '\n' +
+      "Year: " + info.Year + '\n' +
+      "IMDB Rating: " + info.Ratings[0].Value + '\n' +
+      "Country Production: " + info.Country + '\n' +
+      "Language: " + info.Language + '\n' +
+      "Plot: " + info.Plot + '\n' +
+      "Actors: " + info.Actors
+    );
+  }
+})
+movieThis();
 
 
+function doWhatItSays(){
+  fs.readFile("random.txt", "utf8", function(err, data){
+    if (err) {
+      return console.log('An error occured' + err)
+    } else {
+        var dataArr = data.split(',');
+          command = dataArr[0];
+          searchTerm = dataArr[1];
+        switchCommand();
+
+    }
+  });
+};
 
 
 
 
 //--------------------------------------- Request
-
-
 //   request('http://www.google.com', function (error, response, body) {
 //     console.log('error:', error); // Print the error if one occurred
 //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
